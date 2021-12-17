e79ab6d377eebaeccd6b4413adf4906794e03294
OlBul <izotova_o@ukr.net>
2021-12-17 17:58:48 +0200

    Add files via upload

--- source/js/script.js
+++ source/js/script.js
@@ -1,31 +1,47 @@
 let burgerMenu = document.querySelector('.header__burger');
 let headerNav = document.querySelector('.header__nav');
-let body = document.querySelector('.body');
+let body = document.querySelector('body');
 burgerMenu.addEventListener('click', function () {
     burgerMenu.classList.toggle('active');
     headerNav.classList.toggle('active');
     body.classList.toggle('lock');
 })
 
-// Slick Slider =====================================================
-$(document).ready(function () {
-    $('.slider__columns').slick({
-        slidesToShow: 3,
-        adaptiveHeight: true,
-        touchThreshold: 12,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow:2,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow:1,
-                }
-            }
-        ]
-    });
-});
+// Swiper Slider =====================================================
+
+/*const sliderTop = new Swiper('.swiper', {
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    autoHeigth: true,
+    slidesPerView: 3,
+    watchOverflow: true,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    //centeredSlides: true,
+
+});*/
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    //direction: 'vertical',
+    //loop: true,
+  
+   
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    autoHeigth: true,
+    slidesPerView: 3,
+    watchOverflow: true,
+    spaceBetween: 45,
+    slidesPerGroup: 1,
+  
+    
+  });
